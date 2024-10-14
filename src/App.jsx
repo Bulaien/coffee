b56bb0f388a0a8d7841282ee5682b1fa1f0a8d98
brian <brian>
2024-10-14 11:53:52 +0200

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Card from "./Card.jsx";
+import Background from "./bg-cafe.jpg";
+
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json"
+      );
+      const data = await res.json();
+
+      setData(data);
+      console.log(data);
+    }
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="flex flex-col items-center h-4/5">
+        <header className="w-4/5 flex  justify-center">
+          <img src={Background} alt="" />
+        </header>
+
+        <main className="w-4/5 h-[800px] bg-black">d</main>
+      </div>
+      <div className="absolute bg-[#1c1d1f]  top-56 left-96 rounded-xl pb-5 ">
+        <div className="flex flex-col items-center mt-16 mb-24">
+          <h1 className="text-3xl text-white text-center mb-3">
+            Our Collection
+          </h1>
+          <p className="text-gray-300  w-1/2 text-center">
+            Introducing our Coffee Collection, a selection of unique coffees
+            from different roast types and origins, expertly roasted in small
+            batches and shipped fresh weekly.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <button className="text-white bg-[#6F757C] py-2 px-3 rounded-xl">
+            All Products
+          </button>
+          <button className="text-white p-3">Available Now</button>
+        </div>
+        <div className="grid grid-cols-3 gap-8 mt-10">
+          {data.map((date) => (
+            <Card
+              name={date.name}
+              price={date.price}
+              image={date.image}
+              id={date.id}
+              popular={date.popular}
+              votes={date.votes}
+              rating={date.rating}
+              available={date.available}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Star from "./Star.svg";
+import Star_fill from "./Star_fill.svg";
+
+function Card({ id, name, price, image, popular, available, rating, votes }) {
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <div>
+        <img className="rounded-lg" src={image} alt="" />
+      </div>
+
+      <div className="flex w-3/4 justify-between mt-3">
+        <p className="font-bold text-white">{name}</p>
+
+        <p className="bg-[#BEE3CC] px-2 rounded-md">{price}</p>
+      </div>
+      <div className="flex text-left justify-between w-3/4">
+        <div className="flex ">
+          <img src={votes ? { Star_fill } : { Star }} alt="" />
+          <p className="text-white text-bold">{rating}</p>
+          <p className="text-gray-300">
+            {" "}
+            {votes ? "(" + votes + " votes)" : "No ratings"}
+          </p>
+        </div>
+        <p className="text-red-400">{!available ? "Sold out" : ""}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
